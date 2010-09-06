706331f0b98a9f52bb83da12bf69b1a207d6f6ad
Jeff Tilson <jrtilson@gmail.com>
2010-09-06 18:46:43 +0000

    New custom group profile page. Basic nav layout for group content

---
+++ views/default/group-extender/css.php
@@ -0,0 +1,43 @@
+<?php
+	/**
+	 * Group-Extender css
+	 * 
+	 * @package Group-Extender
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Jeff Tilson
+	 * @copyright THINK Global School 2010
+	 * @link http://www.thinkglobalschool.com/
+	 * 
+	 */
+?>
+
+/* tool content boxes on group profile page */
+#group_tools_latest {
+	min-height: 300px;
+	margin-top:20px;
+}
+.group_tool_widget {
+	float: none;
+	margin-right:10px;
+	margin-bottom:40px;
+	min-height:200px;
+	width: 100%;
+
+}
+.group_tool_widget.odd {
+	margin-right:0;
+}
+.group_tool_widget h3 {
+	border-bottom:1px solid #CCCCCC;	
+	background:#e4e4e4;
+	color:#333333;
+	padding:5px 5px 3px 5px;
+	-moz-border-radius-topleft:4px;
+	-moz-border-radius-topright:4px;
+	-webkit-border-top-left-radius:4px;
+	-webkit-border-top-right-radius:4px;
+}
+
+.group-extend .group_tool_widget {
+	display: none;
+}
