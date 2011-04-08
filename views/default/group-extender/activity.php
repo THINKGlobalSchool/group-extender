bd39bf5cbe1c2ae800c286a268de056c51a11a20
jtilson <jtilson@ee84b795-2571-4218-8874-2f4f4fa0a1c4>
2011-04-08 17:46:29 +0000

    Fixes for group discussion module. Code cleaning

--- views/default/group-extender/activity.php
+++ views/default/group-extender/activity.php
@@ -1,46 +1,44 @@
 <?php
-	/**
-	 * Group-Extender activity view
-	 * 
-	 * @package Group-Extender
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Jeff Tilson
-	 * @copyright THINK Global School 2010
-	 * @link http://www.thinkglobalschool.com/
-	 * 
-	 */
-	
-	$limit = get_input("limit", 20);
-	$offset = get_input("offset", 0);
-	$group_guid = $vars['entity']->getGUID();
-	
-	// Sanitise variables -- future proof in case they get sourced elsewhere
-	$limit = (int) $limit;
-	$offset = (int) $offset;
-	$group_guid = (int) $group_guid;
+/**
+ * Group-Extender activity view
+ * 
+ * @package Group-Extender
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
 
-	$entities = elgg_get_entities(array(
-		'container_guids' => $group_guid,
-		'group_by' => 'e.guid'
-	));
+$limit = get_input("limit", 20);
+$offset = get_input("offset", 0);
+$group_guid = $vars['entity']->getGUID();
 
-	$entity_guids = array();
-	foreach ($entities as $entity) {
-		$entity_guids[] = $entity->getGUID();
-	}
+// Sanitise variables -- future proof in case they get sourced elsewhere
+$limit = (int) $limit;
+$offset = (int) $offset;
+$group_guid = (int) $group_guid;
 
-	if (count($entity_guids) > 0) {
-		$river_items = elgg_view_river_items('', $entity_guids, '', '', '', '', $limit);
-		$river_items .= elgg_view('riverdashboard/js');
-	} else {
-		$river_items .= elgg_echo('groups:no_activity');
-	}
-	
-	set_context('activity');	
-	$title .= "<h3>" . elgg_echo('group-extender:groupactivity') . "</h3>";
-	$thewire .= elgg_view("thewire/forms/add", array('group' => $vars['entity'], 'location' => 'referer'));
-	set_context($context);
+$entities = elgg_get_entities(array(
+	'container_guids' => $group_guid,
+	'group_by' => 'e.guid'
+));
 
-	echo $title . $thewire . $river_items;
-	
-?>
+$entity_guids = array();
+foreach ($entities as $entity) {
+	$entity_guids[] = $entity->getGUID();
+}
+
+if (count($entity_guids) > 0) {
+	$river_items = elgg_view_river_items('', $entity_guids, '', '', '', '', $limit);
+	$river_items .= elgg_view('riverdashboard/js');
+} else {
+	$river_items .= elgg_echo('groups:no_activity');
+}
+
+set_context('activity');	
+$title .= "<h3>" . elgg_echo('group-extender:groupactivity') . "</h3>";
+$thewire .= elgg_view("thewire/forms/add", array('group' => $vars['entity'], 'location' => 'referer'));
+set_context($context);
+
+echo $title . $thewire . $river_items;
--- views/default/group-extender/sidebar.php
+++ views/default/group-extender/sidebar.php
@@ -1,19 +1,18 @@
 <?php
-	/**
-	 * Group-Extender custom profile sidebar
-	 * 
-	 * @package Group-Extender
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Jeff Tilson
-	 * @copyright THINK Global School 2010
-	 * @link http://www.thinkglobalschool.com/
-	 * 
-	 */
-	
-	// Some content not available for non-members
-	if ($vars['view_all']) {
-		$content = elgg_view('groups/members',array('entity' => $vars['entity']));
-	}
-	
-	echo $content;
-?>
+/**
+ * Group-Extender custom profile sidebar
+ * 
+ * @package Group-Extender
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
+
+// Some content not available for non-members
+if ($vars['view_all']) {
+	$content = elgg_view('groups/members',array('entity' => $vars['entity']));
+}
+
+echo $content;
