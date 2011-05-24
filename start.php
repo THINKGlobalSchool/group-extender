d283788c983e9530f705a1842893d3ba7f0badb2
jtilson <jtilson@ee84b795-2571-4218-8874-2f4f4fa0a1c4>
2011-05-24 22:18:47 +0000

    Re-wrote group extender to work with Elgg 1.8. No more view overriding grossness.. just clean JS

--- start.php
+++ start.php
@@ -10,51 +10,32 @@
  * 
  */
 
+// Register init
+elgg_register_event_handler('init', 'system', 'group_extender_init');
+
 // Init
 function group_extender_init() {
 	
-	// Un-register the groups page handler so I can mess with it
-	unregister_page_handler('groups');
+	// Register typeaheadtags JS
+	$extender_js = elgg_get_simplecache_url('js', 'group-extender/extender');
+	elgg_register_js('elgg.groupextender', $extender_js);
 	
 	// Register my own page handler
-	register_page_handler('groups','group_extender_page_handler');
+	elgg_register_page_handler('groups','group_extender_page_handler');
 	
 	// CSS
-	elgg_extend_view('css/screen', 'group-extender/css');
+	elgg_extend_view('css/elgg', 'css/group-extender/css');
 }
 
 /**
- * Group extender page handler
+ * Group extender page handler, loads the JS and calls the regular group page handler
  *
  * @param array $page Array of page elements, forwarded by the page handling mechanism
  */
 function group_extender_page_handler($page) {
-	if (isset($page[0])) {	
-		// If we've got a int (guid), load up custom group page
-		if ($group = get_entity((int)$page[0])) {
-			
-			set_page_owner($group->getGUID());
-			
-			// Hide some items from closed groups when the user is not logged in.
-			$view_all = true;
-
-			$groupaccess = group_gatekeeper(false);
-			if (!$groupaccess) { 
-				$view_all = false;
-			}
-			
-			$title = $group->name;
-			$sidebar = elgg_view('group-extender/sidebar', array('view_all' => $view_all, 'entity' => $group));
-			
-			$content = elgg_view('navigation/breadcrumbs') . elgg_view('group-extender/maincontent', array('view_all' => $view_all, 'entity' => $group));
-			
-			$body = elgg_view_layout('one_column_with_sidebar', $content, $sidebar);
-			
-			echo elgg_view_page($title, $body);
-		} else {
-			groups_page_handler($page);
-		}
-	}
+		// Load extender JS
+		elgg_load_js('elgg.groupextender');
+		groups_page_handler($page);
+		return true;
 }
 	
-elgg_register_event_handler('init', 'system', 'group_extender_init');
---
+++ views/default/js/group-extender/extender.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Typeahead Tags JS
+ * 
+ * @package Group-Extender
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ */
+?>
+//<script>
+elgg.provide('elgg.groupextender');
+
+elgg.groupextender.init = function() {	
+
+	// Add a click handler for the tabs
+	$('a.group-tools-item-link').live('click', elgg.groupextender.switchGroupToolsTab);
+
+	// When ready, hack the divs
+	$(function() {
+			var groupToolsDiv = $('#groups-tools');
+			
+			groupToolsDiv.prepend('<ul id="group-tools-tabbed-nav" class="elgg-menu elgg-menu-filter elgg-menu-hz elgg-menu-filter-default"></ul>');
+	
+			groupToolsDiv.children("div.elgg-module").each(function() {
+					// Hide the module
+					$(this).hide();
+				
+					// Clean up extra classes
+					$(this).removeClass('odd');
+
+					// Get title
+					var title = $('h3', this).html();
+					
+					// Get a code-friendly id for the modules (lowercase, spaces replaced with -)
+					var module_id = title.toLowerCase().replace(' ', '-');
+					
+					// Add the nav item	
+					$('#group-tools-tabbed-nav').append('<li class="group-tools-item"><a class="group-tools-item-link" href="#group-tools-' + module_id + '">' + title + '</a></li>');
+					// Add an id and class to the module
+					$(this).attr('id', 'group-tools-' + module_id);
+					$(this).addClass('group-tools-module');
+			});	
+			
+			// Fire the click handler for the first nav item
+			groupToolsDiv.find("ul#group-tools-tabbed-nav li:first-child a").click();
+
+	});
+}
+
+// Click handler for group tools nav items
+elgg.groupextender.switchGroupToolsTab = function(event) {
+	// Hide all modules
+	$('.group-tools-module').hide();
+	
+	// Remove selected class
+	$('#group-tools-tabbed-nav').children("li").each(function() {$(this).removeClass('elgg-state-selected');});
+	
+	// Add selected class to this item
+	$(this).parent().addClass('elgg-state-selected');
+	
+	// Show the div (passed as href)
+	$($(this).attr('href')).show();
+	
+	event.preventDefault();
+}
+
+elgg.register_hook_handler('init', 'system', elgg.groupextender.init);
+//</script>
