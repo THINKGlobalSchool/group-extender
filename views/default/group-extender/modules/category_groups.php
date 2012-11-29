a3f06ff06dbd6a6c1dfd2ca5ec716dddfcb7a4c7
Jeff Tilson <jrtilson@gmail.com>
2012-11-29 21:53:25 +0000

    Excluding archived groups from the 'all groups' listing

--- views/default/group-extender/modules/category_groups.php
+++ views/default/group-extender/modules/category_groups.php
@@ -14,11 +14,31 @@ $guid = elgg_extract('guid', $vars, NULL);
 
 // Load All Groups
 if ($guid == 'all') {
-	$content = elgg_list_entities(array(
+	$options = array(
 		'types' => 'group',
 		'full_view' => FALSE,
 		'limit' => 15,
-	));
+	);
+
+	// If we have an archived category, exclude those groups from the all listing
+	if ((int)elgg_get_plugin_setting('archive_category', 'group-extender')) {
+		global $CONFIG;
+
+		// MD info for excluding archived
+		$archived = get_metastring_id('archived');
+		$one_id = get_metastring_id(1);
+
+		if ($archived && $one_id) {
+					$options['wheres'] = "
+		  			NOT EXISTS (
+						SELECT 1 FROM {$CONFIG->dbprefix}metadata md
+						WHERE md.entity_guid = e.guid
+						AND md.name_id = $archived
+						AND md.value_id = $one_id)";
+		}
+	}
+
+	$content = elgg_list_entities_from_metadata($options);
 } else {
 	$category = get_entity($guid);
 
